a4bd46b97ea8425673858354a4dd9721ec588421
Tommy18062001 <johantony63@gmail.com>
2022-10-06 08:39:12 +0300

    Update the swagger documentation

---
+++ modules/auth.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// create a model for each student
+const userSchema = new mongoose.Schema({
+    id: { type: String},
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+})
+
+module.exports = mongoose.model('user', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 
 routes.use('/students', require('./students'))
+routes.use('/auth', require('./auth'))
 
 module.exports = routes;
