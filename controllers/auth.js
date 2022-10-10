a501e86fd7ffaca0391154568cdeb6fe1d796e2c
Tommy18062001 <johantony63@gmail.com>
2022-10-10 17:29:26 +0300

    fix registration issue

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,32 +3,32 @@ const bcrypt = require("bcryptjs");
 
 // POST REQUEST
 const registerUser = async (req, res, next) => {
-  console.log(req.body.name);
   const newUser = new User({ 
     name: req.body.name,
     email: req.body.email, 
     password: req.body.password
   });
+
+  // console.log(newUser);
   // Hash passworefore saving in database
   bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
-
           newUser.password = hash;
           newUser.save()
               .then(user => {
                 res.status(201).json(user);
               })
               .catch(err => {
-                res.status(500).json({ err: err });
+                res.status(500).json({ err: "this is my" + err });
               });
       });
   });
   
 };
 
-  module.exports = {
-    registerUser
-  }
+module.exports = {
+  registerUser
+}
 
   
